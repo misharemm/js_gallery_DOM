3c57725754c547cd10c22e3c35b6a7b478f36b0f
Misha Remeslo <misha.remeslol@gmail.com>
2024-07-25 12:56:39 +0300

    make build

---
+++ index.f2453f43.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");Array.from(document.querySelectorAll("a")).forEach(function(r){r.addEventListener("click",function(t){t.preventDefault(),e.src=r.href})});
+//# sourceMappingURL=index.f2453f43.js.map
